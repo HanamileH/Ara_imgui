a4b766accbfaf497c9eff39840645858f0a3806c
HanamileH <hanamileh@gmail.com>
2026-06-08 21:41:54 +0300

    removed all files from gh-pages branch

--- src/ara_imgui/__init__.py
+++
@@ -1,5 +0,0 @@
-import imgui
-from .app import App, AraImgui
-from .window import Window
-
-__all__ = ['App', 'Window', 'AraImgui', 'imgui']
--- tests/test_basic.py
+++
@@ -1,3 +0,0 @@
-# Test if the module can be imported
-def test_import():
-   import ara_imgui
